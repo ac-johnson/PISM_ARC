b91f82847cc739e251749606ab7208de1f61b33f
ac-johnson <acjohnson149@live.com>
2018-03-01 11:08:38 +1300

    test

--- firstrun/callrun.sh
+++ firstrun/callrun.sh
@@ -1,7 +1,7 @@
 export NN=8
-export Inspin=~/maps/end_evol-5km_Ant_spinup_W65.nc
-export Inboot=~/maps/PISM_1km_v4.nc
+export Inspin=$HOME/maps/end_evol-5km_Ant_spinup_W65.nc
+export Inboot=$HOME/maps/PISM_1km_v4.nc
 export Yst=1
-export Outfile=~/maps/Ant_1yr_fk.nc
+export Outfile=$HOME/maps/Ant_1yr_fk.nc
 
 ./dorun.sh
