3a663f1aff9d79b29affb886a5f3c6d5d9f603f3
ac-johnson <acjohnson149@live.com>
2018-03-01 10:59:17 +1300

    is this necessary?

--- firstrun/callrun.sh
+++ firstrun/callrun.sh
@@ -1,6 +1,6 @@
 export NN=8
-export Inspin=../end_evol-5km_Ant_spinup_W65.nc
-export Inboot=../PISM_1km_v4.nc
+export Inspin=~/maps/end_evol-5km_Ant_spinup_W65.nc
+export Inboot=~/maps/PISM_1km_v4.nc
 export Yst=1
 export Outfile=~/maps/Ant_1yr_fk.nc
 
