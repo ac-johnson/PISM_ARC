62d82aec4472013dec3b7e52e9e65dddc5959ae6
ac-johnson <acjohnson149@live.com>
2018-03-01 16:00:56 +1300

    yes

--- firstrun/callrunCHINOOK.sh
+++ firstrun/callrunCHINOOK.sh
@@ -2,6 +2,7 @@
 export Inspin=$CENTER1/map/end_evol-5km_Ant_spinup_W65.nc
 export Inboot=$CENTER1/map/PISM_1km_v4.nc
 export Yst=1
-export Outfile=$CENTER1/runs/Ant_1yr_fk.nc
+export Outloc=$CENTER1/runs/
+export Outfm=Ant_1yr_fk.nc
 
 ./dorunCHINOOK.sh
--- firstrun/dorun.sh
+++ firstrun/dorun.sh
@@ -83,6 +83,9 @@ else bedfstr="-bed_def "$bdef; fi
 #mx= 	1157
 #my=	994
 
+#alternative mx: 290,576
+#alternative my: 245,497
+
 mpiexec -n $NN pismr -i $Inboot -bootstrap \
   -regrid_file $Inspin \
   -regrid_vars bwat,bmelt,dbdt,litho_temp,mask,temp_pa,enthalpy \
@@ -103,9 +106,10 @@ mpiexec -n $NN pismr -i $Inboot -bootstrap \
   -subgl $nsbm $bdefstr \
   -tauc_slippery_grounding_lines -ts_file "${Outloc}ts_${Outfm}" -ts_times -$Yst:yearly:0 \
   -extra_file "${Outloc}ex_${Outfm}" -extra_times -$Yst:50:0 \
-  -extra_vars diffusivity,temppabase,tempicethk_basal,bmelt,tillwat,velsurf_mag,mask,thk,topg,usurf,hardav,velbase_mag,tauc,strain_rates,tendency_of_ice_amount_due_to_discharge,deviatoric_stresses,dHdt \
+  -extra_vars diffusivity,temppabase,tempicethk_basal,bmelt,tillwat,velsurf_mag,mask,thk,topg,usurf,hardav,velbase_mag,tauc,tendency_of_ice_amount_due_to_discharge,dHdt \
   -o "$Outloc$Outfm"
 
 
+#not getting: strain_rates, deviatoric_stresses
 
 #mpiexec -n 8 pismr -i ../end_evol-5km_Ant_spinup_W65.nc -bootstrap -Mx 301 -My 561 -Mz 201 -Mbz 21 -z_spacing equal -Lz 4000 -Lbz 2000 -skip -skip_max 20 -grid.correct_cell_areas false -grid.registration corner -ys -500 -ye 0 -surface given -surface_given_file pism_Greenland_5km_v1.1.nc -calving ocean_kill -ocean_kill_file pism_Greenland_5km_v1.1.nc -sia_e 3.0 -stress_balance ssa+sia -topg_to_phi 15.0,40.0,-300.0,700.0 -pseudo_plastic -pseudo_plastic_q 0.25 -till_effective_fraction_overburden 0.02 -tauc_slippery_grounding_lines -ts_file ts_test.nc -ts_times -500:yearly:0 -extra_file ex_test.nc -extra_times -500:100:0 -extra_vars diffusivity,temppabase,tempicethk_basal,bmelt,tillwat,velsurf_mag,mask,thk,topg,usurf,hardav,velbase_mag,tauc -o test.nc
